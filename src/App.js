b2af385875b91ab5a27ae92262ef259baa7cf054
arnebree-maker <arnebree@gmail.com>
2025-11-20 09:37:35 +0100

    Change LinkIcon color to black in buttons

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ export default function App() {
                     <p className="text-xs font-bold mb-2 tracking-wide">COLLECTIEVE INSPIRATIE!</p>
                     <p className="text-sm mb-4">Bekijk wat je collega's hebben gemaakt en voeg jouw beste ideeën toe!</p>
                     <Button as="a" href="https://drive.google.com/drive/folders/12hkdkdgrNgK8W3b5qXjJvcPaKsinLEuj?usp=drive_link" className="w-full bg-white text-orange-600 hover:bg-orange-50 shadow-md">
-                      <LinkIcon className="h-4 w-4" /> Drop je eigen ideeën hier
+                      <LinkIcon className="h-4 w-4 text-black" /> Drop je eigen ideeën hier
                     </Button>
                   </div>
                 </div>
@@ -276,7 +276,7 @@ export default function App() {
                 <h4 className="font-bold text-white mb-3 text-lg">Deel je ervaring!</h4>
                 <p className="text-sm text-white/95 mb-4">Heb je zelf interessante projecten of toepassingen in je les? Deel je ideeën en ervaringen met de hele werkgroep. Dit helpt iedereen om van elkaar te leren!</p>
                 <Button as="a" href="https://drive.google.com/drive/folders/12hkdkdgrNgK8W3b5qXjJvcPaKsinLEuj?usp=drive_link" className="w-full bg-black text-blue-700 hover:bg-blue-50 shadow-lg">
-                  <LinkIcon className="h-4 w-4" /> Drop je projectjes in de Google Drive
+                  <LinkIcon className="h-4 w-4 text-black" /> Drop je projectjes in de Google Drive
                 </Button>
               </div>
             </div>
